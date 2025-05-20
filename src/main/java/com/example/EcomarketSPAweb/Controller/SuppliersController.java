77237ab57975fe64181bbf1ccc7174dd70f35f55
CETECOM <ca.calvio@duocuc.cl>
2025-05-20 09:12:53 -0400

    se arreglo spupliers

--- src/main/java/com/example/EcomarketSPAweb/Controller/SuppliersController.java
+++ src/main/java/com/example/EcomarketSPAweb/Controller/SuppliersController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/suppliers")
+
 public class SuppliersController {
     @Autowired
     private SuppliersService supplierService;
