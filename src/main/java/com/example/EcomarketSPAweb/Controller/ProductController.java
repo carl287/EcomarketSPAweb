442b4e6ed7e03987edddd4fbcad0c91564db16aa
CETECOM <ca.calvio@duocuc.cl>
2025-06-03 10:08:49 -0400

    se documento el controlador de product

--- src/main/java/com/example/EcomarketSPAweb/Controller/ProductController.java
+++ src/main/java/com/example/EcomarketSPAweb/Controller/ProductController.java
@@ -3,11 +3,14 @@ package com.example.EcomarketSPAweb.Controller;
 import com.example.EcomarketSPAweb.Model.Product;
 import com.example.EcomarketSPAweb.Services.GestionProductService;
 import com.example.EcomarketSPAweb.Services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Vista de productos")
 //probando
 public class ProductController {
 
@@ -17,10 +20,12 @@ public class ProductController {
     private GestionProductService gestionProductService;
 
     @GetMapping
+    @Operation(summary = "Se lista todos los productos")
     public String getProduct() {return productService.listarProductos();}
 
 
     @GetMapping("/{id}")
+    @Operation(summary = "Se busca el producto por id")
     public String getProductById(@PathVariable int id) {
         return productService.obtenerProductoPorId(id);
     }
