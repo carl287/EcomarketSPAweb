f141b2196ad99fb47c5a46e409d887b693a775fe
carl287 <ca.calvio@duocuc.cl>
2025-06-06 14:58:38 -0400

    se agrego el test de usuario
    agregue el test de usaurio y funciona, esta denuevo el problema de que devuelve nulo en los provedoores

--- src/test/java/com/example/EcomarketSPAweb/EcomarketSpAwebUserTest.java
+++ src/test/java/com/example/EcomarketSPAweb/EcomarketSpAwebUserTest.java
@@ -1,11 +1,11 @@
 package com.example.EcomarketSPAweb;
 
-import com.example.EcomarketSPAweb.Model.GestionProduct;
 import com.example.EcomarketSPAweb.Model.Product;
-import com.example.EcomarketSPAweb.Repository.GestionProductRepository;
+import com.example.EcomarketSPAweb.Model.User;
 import com.example.EcomarketSPAweb.Repository.ProductRepository;
-import com.example.EcomarketSPAweb.Services.GestionProductService;
+import com.example.EcomarketSPAweb.Repository.UserRepository;
 import com.example.EcomarketSPAweb.Services.ProductService;
+import com.example.EcomarketSPAweb.Services.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,30 +18,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class EcomarketSpAwebProductTest {
+class EcomarketSpAwebUserTest {
 
 	@Autowired
-	ProductRepository productRepository;
+	UserRepository userRepository;
 
 	@MockitoBean
-	ProductService productService;
+	UserService userService;
 
 	@Test
 	@DisplayName("FindAll Test")
-	void testProductServiceMock() {
-		List<Product> productos = productRepository.findAll();
-		assertNotNull(productos);
-		assertEquals(3, productos.size());
+	void testUserServiceMock () {
+		List<User> users = userRepository.findAll();
+		assertNotNull(users);
+		//se coloca cuantos usuarios estan registrados en la base de datos//
+		assertEquals(1, users.size());
 	}
 
 	@Test
-	@DisplayName("Rectificar precio producto")
-	void testFindProduct(){
-		Product prueba = productRepository.findById(1).get();
+	@DisplayName("Rectificar nombre usuario")
+	void testFindUser(){
+		User prueba = userRepository.findById(2).get();
 		assertNotNull(prueba);
-		assertEquals(1990,prueba.getPrice());
+		//se coloca el nombre del usuario para verificar si exsite en la base de datos//
+		assertEquals("usuario1", prueba.getUsername());
 	}
 
 
-	/* Si pueden agregar los test para los demas controladores por favor */
 }
