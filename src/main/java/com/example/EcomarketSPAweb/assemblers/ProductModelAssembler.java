baf48d8ab6c43d21f17b5cc927ecab9d0a1e93a1
carl287 <ca.calvio@duocuc.cl>
2025-06-16 11:22:40 -0400

    agregue lo que faltaba en  ProductModelAssembler

--- src/main/java/com/example/EcomarketSPAweb/assemblers/ProductModelAssembler.java
+++ src/main/java/com/example/EcomarketSPAweb/assemblers/ProductModelAssembler.java
@@ -1,9 +1,25 @@
 package com.example.EcomarketSPAweb.assemblers;
 
 
+import com.example.EcomarketSPAweb.Controller.ProductController;
+import com.example.EcomarketSPAweb.Model.GestionProduct;
+import com.example.EcomarketSPAweb.Model.Product;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
-public class ProductModelAssembler {
+public class ProductModelAssembler implements RepresentationModelAssembler<Product, EntityModel<Product>> {
+    @Override
+    public EntityModel<Product> toModel(Product product) {
+        return EntityModel.of(product,
+                linkTo(methodOn(ProductController.class).getProductById(product.getId())).withSelfRel(),
+                linkTo(methodOn(ProductController.class).getProduct()).withRel("product")
+        );
+    }
 
 }
